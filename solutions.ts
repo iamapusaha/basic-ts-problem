a42bbc1ba1827dea10ec858d2b9728ed6e900300
iamapusaha <apusaha392@gmail.com>
2026-05-07 09:40:22 +0600

    added 3rd problem

--- solutions.ts
+++ solutions.ts
@@ -5,8 +5,20 @@ const filterEvenNumbers = (input: number[]): number[] => {
 
 // console.log(filterEvenNumbers([1, 3, 8, 7, 5, 96, 12]));
 
+//problem 02
 const reverseString = (input: string): string => {
   return input.split("").reverse().join("");
 };
 
 // console.log(reverseString("typescript"));
+
+//problem 03
+type StringOrNumber = string | number;
+const checkType = (input: StringOrNumber): string => {
+  if (typeof input === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+};
+// console.log(checkType(null));
