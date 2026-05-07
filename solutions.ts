1897a7890a05d8a56ab923fa4ee9bc941973cfc7
iamapusaha <apusaha392@gmail.com>
2026-05-07 09:12:12 +0600

    added 2nd problem

--- solutions.ts
+++ solutions.ts
@@ -3,4 +3,10 @@ const filterEvenNumbers = (input: number[]): number[] => {
   return input.filter((e) => e % 2 === 0);
 };
 
-console.log(filterEvenNumbers([1, 3, 8, 7, 5, 96, 12]));
+// console.log(filterEvenNumbers([1, 3, 8, 7, 5, 96, 12]));
+
+const reverseString = (input: string): string => {
+  return input.split("").reverse().join("");
+};
+
+// console.log(reverseString("typescript"));
